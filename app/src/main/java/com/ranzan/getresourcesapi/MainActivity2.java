d5e20a306425c283f34614d8d17c80bc5279502f
PRAKASH RANJAN <aryaprakashranjan@gmail.com>
2021-07-30 22:38:57 +0530

    progress bar done

--- app/src/main/java/com/ranzan/getresourcesapi/MainActivity2.java
+++ app/src/main/java/com/ranzan/getresourcesapi/MainActivity2.java
@@ -2,7 +2,10 @@ package com.ranzan.getresourcesapi;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import androidx.appcompat.app.ActionBar;
@@ -17,7 +20,9 @@ public class MainActivity2 extends AppCompatActivity {
     private TextView num;
     private TextView year;
     private TextView company;
+    private RelativeLayout relativeLayout;
     private LinearLayout linearLayout;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,16 +31,19 @@ public class MainActivity2 extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
         initViews();
-        int value =Integer.parseInt(getIntent().getStringExtra("value"));
+        linearLayout.setVisibility(View.GONE);
+        int value = Integer.parseInt(getIntent().getStringExtra("value"));
         num.setText(value + "");
         APIClient apiClient = Network.getRetrofitInstance().create(APIClient.class);
         apiClient.getPost(value).enqueue(new Callback<ResponseModel>() {
             @Override
             public void onResponse(Call<ResponseModel> call, Response<ResponseModel> response) {
+                linearLayout.setVisibility(View.VISIBLE);
+                progressBar.setVisibility(View.GONE);
                 ResponseModel model = response.body();
                 year.setText(model.getData().getYear()+"");
                 name.setText(model.getData().getName());
-                linearLayout.setBackgroundColor(Color.parseColor(model.getData().getColor()));
+                relativeLayout.setBackgroundColor(Color.parseColor(model.getData().getColor()));
                 company.setText(model.getData().getPantoneValue());
             }
 
@@ -51,6 +59,9 @@ public class MainActivity2 extends AppCompatActivity {
         name = findViewById(R.id.name);
         year = findViewById(R.id.year);
         company = findViewById(R.id.company);
-        linearLayout = findViewById(R.id.layout);
+        relativeLayout = findViewById(R.id.layout);
+        linearLayout = findViewById(R.id.visibleLayout);
+        progressBar = findViewById(R.id.progressBar);
+
     }
 }
